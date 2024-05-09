e86a9fa15276abb724229c9548d9a9100e8bcd6e
giru bhai <girubhai12@gmail.com>
2024-05-09 19:50:23 +0545

    project done

---
+++ src/data/items.js
@@ -0,0 +1,105 @@
+export const items = [
+
+
+  {
+    id:1,
+    name:"Sapu Mhicha",
+    category:"newari",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Sapu_Micha.jpg/1200px-Sapu_Micha.jpg",
+    price:"400",
+  },
+  {
+    id:2,
+    name:"Chatamari",
+    category:"newari",
+    image:"https://newarirecipeshut.com/wp-content/uploads/2020/09/chatamari-768x512.jpg",
+    price:"250",
+  },
+  {
+    id:3,
+    name:"Batuk",
+    category:"magar",
+    image:"https://ecs.com.np/fckimage/article/images/2017/2/food2.jpg",
+		price:"100"
+  },
+  {
+    id:4,
+    name:"Pakuwa",
+    category:"tharu",
+    image:"https://www.thegundruk.com/wp-content/uploads/2016/01/IMG_0132-300x200.jpg",
+    price:"220"
+  },
+  {
+    id:5,
+    name:"Bara",
+    category:"newari",
+    image:"https://newarirecipeshut.com/wp-content/uploads/2020/09/bara-1-768x512.jpg",
+    price:"200",
+  },
+  {
+    id:6,
+    name:"Yomari",
+    category:"newari",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Yamari.jpg/360px-Yamari.jpg",
+    price:"150"
+  },
+
+  {
+    id:7,
+    name:"Shyakpa",
+    category:"sherpa",
+    image:"https://res.cloudinary.com/images-swotahtravel-com/image/upload/v1695288755/blog%20images/shakpa.jpg",
+    price:"150"
+  },
+  {
+    id:8,
+    name:"Chhurpi Soup",	
+    category:"sherpa",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ39ujJAW5Zg2RoiOlHUMbSuYLcHo23Bktss_33IgYGUvSl7Wrya2MaMllU_4DB5MhNeXo&usqp=CAU",
+    price:"140"
+  },
+  {
+    id:9,
+    name:"Sishnu Soup",
+    category:"sherpa",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/St_John_Restaurant%2C_Smithfield%2C_London_%283445306202%29.jpg/330px-St_John_Restaurant%2C_Smithfield%2C_London_%283445306202%29.jpg",
+    price:"170"    
+  },
+  {
+    id:10,
+    name:"Ghonghi",
+    category:"tharu",
+		image:"https://nepal8thwonder.com/wp-content/uploads/2021/09/Ghongi-Tharu-Cuisine-768x768.jpg",
+    price:"200"
+  },
+  {
+    id:11,
+    name:"dhikri",
+    category:"tharu",
+    image:"https://www.century-foods.com/wp-content/uploads/2022/09/Dhikri.jpg",
+    price:"210"
+  },
+  {
+    id:12,
+    name:"sidhar",
+    category:"tharu",
+    image:"https://www.thegundruk.com/wp-content/uploads/2016/01/Sidhara.jpg",
+    price:"240",
+
+  }
+
+];
+
+
+
+
+
+export const specialItem = [
+  {
+    id: 1,
+    name: "Rice With Zucchini, Soft Boiled Egg, and Parsley",
+    image:
+      "https://images.pexels.com/photos/1410235/pexels-photo-1410235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "2000",
+  },
+];
